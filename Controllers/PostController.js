57b03aa62abd4ac70e734c9d498dbc284d39339d
carsalcedo <fercarrera07@gmail.com>
2023-01-25 12:23:27 -0400

    publicacion d eimagenes y likeslistos

--- Controllers/PostController.js
+++ Controllers/PostController.js
@@ -73,24 +73,22 @@ export const deletePost = async (req, res) =>{
 
 //like a post
 
-export const likepost = async (req, res) =>{
-    const id= req.params.id
-    const {userId} = req.body
-
+export const likepost = async (req, res) => {
+    const id = req.params.id;
+    const { userId } = req.body;
     try {
-       const post = await PostModel.findById(id)
-       if(!post.likes.includes(userId)){
-            await post.updateOne({$push : {likes : userId}})
-            res.status(200).json("Post liked")
-        }else{
-            await post.updateOne({$pull : {likes : userId}})
-            res.status(200).json("Post unliked") 
-        }
-
+      const post = await PostModel.findById(id);
+      if (post.likes.includes(userId)) {
+        await post.updateOne({ $pull: { likes: userId } });
+        res.status(200).json("Post disliked");
+      } else {
+        await post.updateOne({ $push: { likes: userId } });
+        res.status(200).json("Post liked");
+      }
     } catch (error) {
-        res.status(500).json(error)
+      res.status(500).json(error);
     }
-}
+  };
 
 //Get timeline post
 
--- index.js
+++ index.js
@@ -11,6 +11,10 @@ import UploadRoute from "./Routes/UploadRoute.js"
 
 const app = express();
 
+//to serve iamges for public
+app.use(express.static('public'))
+app.use('/images', express.static('images'))
+
 //Middlewares
 app.use(bodyParser.json({limit: '30mb', extended: true}));
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
