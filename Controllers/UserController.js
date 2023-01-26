10b12c08417a1d99c16dc8b204043384b2cb992e
carsalcedo <fercarrera07@gmail.com>
2023-01-26 13:22:16 -0400

    user update mejorado

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,5 +1,6 @@
 import UserModel from "../Models/userModel.js";
 import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken'
 //get a User
 
 export const getUser = async (req, res)=>{
@@ -23,26 +24,37 @@ export const getUser = async (req, res)=>{
 //update user
 export const updateUser = async (req, res) => {
     const id = req.params.id;
-    const {currentUserId, currentUserAdminStatus, password} = req.body;
-
-    if(id===currentUserId || currentUserAdminStatus){
-        try {
-
-            if(password){
-                const salt = await bcrypt.genSalt(10)
-                req.body.password = await bcrypt.hash(password, salt)
-            }
-
-            const user = await UserModel.findByIdAndUpdate(id, req.body, {new:true})
-            res.status(200).json(user)
-            
-        } catch (error) {
-            res.status(500).json(error)
+    // console.log("Data Received", req.body)
+    const { _id, currentUserAdmin, password } = req.body;
+    
+    if (id === _id) {
+      try {
+        // if we also have to update password then password will be bcrypted again
+        if (password) {
+          const salt = await bcrypt.genSalt(10);
+          req.body.password = await bcrypt.hash(password, salt);
         }
-    }else{
-        res.status(403).json("Access Denied")
+        // have to change this
+        const user = await UserModel.findByIdAndUpdate(id, req.body, {
+          new: true,
+        });
+        const token = jwt.sign(
+          { username: user.username, id: user._id },
+          process.env.JWT_KEY,
+          { expiresIn: "1h" }
+        );
+        console.log({user, token})
+        res.status(200).json({user, token});
+      } catch (error) {
+        console.log("Error agya hy")
+        res.status(500).json(error);
+      }
+    } else {
+      res
+        .status(403)
+        .json("Access Denied! You can update only your own Account.");
     }
-}
+  };
 
 //Delete user
 
